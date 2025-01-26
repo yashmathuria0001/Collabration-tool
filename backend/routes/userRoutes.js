156f2232e4453214813fb78bed0590646176e2fb
Esha Raj <eshhacb@gmail.com>
2025-01-26 12:23:07 +0530

    OTP feature

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { registerUser, login, logout } from '../controllers/userController.js';
+import { registerUser, login, logout, resendOTP, verify } from '../controllers/userController.js';
 
 const router= express.Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(login);
+router.route('/resendOTP').post(resendOTP);
+router.route('/verify').post(verify);
 router.route("/logout").get(logout);
 
 export default router;
